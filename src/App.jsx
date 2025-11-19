a1592155fb140c46d0a10d353bcd88a7eae2e8df
asif is here <asifafran24@gmail.com>
2025-11-19 19:58:06 +0600

    particles

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import React , { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkFrom from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css'
+import ParticlesBg from 'particles-bg';
 
 
 class App extends Component {
   render () { 
     return (
-    <div>
-      <Navigation/>
-      <Logo/>
-      <ImageLinkFrom/>
-      {/*<FaceRecognization/> */}
-    </div>
+      <>
+        <div>
+          <Navigation/>
+          <Logo/>
+          <Rank/>
+          <ImageLinkFrom/>
+          {/*<FaceRecognization/> */}
+        </div>
+        <ParticlesBg type="cobweb" color="#ffffff" num={200} bg={true} />
+      </>
     )
   }
 }
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return(
+        <div className=''>
+            <div className=' white f3'>
+                {'Asif, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#7'}
+            </div>
+
+        </div>
+    );
+}
+
+export default Rank;
