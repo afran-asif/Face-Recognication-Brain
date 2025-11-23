5a5ea29d540c8bb29db29b90683aea115f01ee26
asif is here <asifafran24@gmail.com>
2025-11-23 22:13:31 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,97 @@
-import React , { Component } from 'react';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkFrom from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognization from './components/FaceRecognization/FaceRecognization';
-import './App.css'
-import ParticlesBg from 'particles-bg';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkFrom from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognization from "./components/FaceRecognization/FaceRecognization";
+import "./App.css";
+import ParticlesBg from "particles-bg";
 
+// Clarifai model info
+const MODEL_ID = "face-detection";  
+const PAT = import.meta.env.VITE_CLARIFAI_API_KEY;
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
     this.state = {
-      input : '',
-      imgUrl : '',
-    }
+      input: "",
+      imgUrl: "",
+    };
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value });
-  }
-  
+    this.setState({ input: event.target.value });
+  };
+
   onSubmitButton = async () => {
-    this.setState({ imgUrl : this.state.input })
+    this.setState({ imgUrl: this.state.input });
 
     const raw = JSON.stringify({
-      "model_id": "face-detection",
-      "inputs": [
+      inputs: [
         {
-          "data": {
-            "image": {
-              "url": this.state.input
-            }
-          }
-        }
-      ]
+          data: {
+            image: {
+              url: this.state.input,
+            },
+          },
+        },
+      ],
     });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
 
     try {
       const response = await fetch(
-        "https://api.clarifai.com/v2/models/face-detection/outputs",
-        {
-          method: "POST",
-          headers: {
-            "Accept": "application/json",
-            "Authorization": `Key ${import.meta.env.VITE_CLARIFAI_API_KEY}`
-          },
-          body: raw
-        }
+        `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`
+,
+        requestOptions
       );
-
       const result = await response.json();
-      console.log(result.outputs[0].data);
+
+      console.log("Clarifai Result:", result);
+
+      const regions = result.outputs?.[0]?.data?.regions;
+      if (!regions) {
+        console.log("No face detected");
+        return;
+      }
+
+      regions.forEach((region) => {
+        const box = region.region_info.bounding_box;
+
+        console.log(
+          `Bounding Box: ${box.top_row}, ${box.left_col}, ${box.bottom_row}, ${box.right_col}`
+        );
+      });
     } catch (error) {
       console.error("Clarifai Error:", error);
     }
-};
+  };
 
-  
-  render(){
+  render() {
     return (
       <>
         <div>
-          <Navigation/>
-          <Logo/>
-          <Rank/>
-          <ImageLinkFrom onInputChange={this.onInputChange} onSubmitButton={this.onSubmitButton}/>
-          <FaceRecognization imgUrl={this.state.imgUrl}/>
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkFrom
+            onInputChange={this.onInputChange}
+            onSubmitButton={this.onSubmitButton}
+          />
+          <FaceRecognization imgUrl={this.state.imgUrl} />
         </div>
         <ParticlesBg type="cobweb" color="#ffffff" num={200} bg={true} />
       </>
-    )
+    );
   }
 }
 
